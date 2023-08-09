76804232b6727d46756d433dc581424f08e9000c
Leckller <7leckler@gmail.com>
2023-08-09 14:30:37 -0300

    gatinhooos

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import './style.css'
 
-const botoes = document.querySelectorAll('button')
+const botoes = document.querySelectorAll('button');
+const imgHTML = document.querySelector('#petImg');
+
+botoes[0].addEventListener('click', (e) => {
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((res) => res.json())
+    .then((res) => {
+      imgHTML.src = res[0].url;
+      console.log('new cat!')
+    })
+});
